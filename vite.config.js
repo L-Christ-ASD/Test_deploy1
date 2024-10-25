7fb6e40704acfdb9834ec8375dd9649dce275a15
L-Christ-ASD <christ.devops75@gmail.com>
2024-10-25 10:02:32 +0200

    config vit pour github

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelteTesting } from '@testing-library/svelte/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/devops-s03-deploy/', // nom du dépôt
+  base: '/Test_deploy1/', // nom du dépôt
   plugins: [
     svelte(),
     svelteTesting()
